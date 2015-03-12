eb0ebf14ee27b2700913e2fd9929e4f82f557e9e
alamer <ivc_ShherbakovIV@IVC-SHHERBAKOVI.esrr.oao.rzd>
2015-03-12 08:26:02 +0300

    + command line param -t number_of_threads

--- src/main/java/ru/jeene/zapretparser/App.java
+++ src/main/java/ru/jeene/zapretparser/App.java
@@ -14,6 +14,11 @@ package ru.jeene.zapretparser;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
 import ru.jeene.zapretparser.controller.CSVLoadController;
 import ru.jeene.zapretparser.controller.XLSXReportController;
 import ru.jeene.zapretparser.models.FullReport;
@@ -28,9 +33,16 @@ public class App {
 
     private static App app;
     private static final org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(App.class);
-    private final int THREAD_NUMBER = 10;
+    @Option(name = "-t")        // no usage
+    private  int THREAD_NUMBER = 10;
 
-    public App() {
+    public App(String[] args) {
+        CmdLineParser parser = new CmdLineParser(this);
+        try {
+            parser.parseArgument(args);
+        } catch (CmdLineException ex) {
+            logger.error(ex);
+        }
         logger.info("Loading CSV file....");
         CSVLoadController loader = new CSVLoadController();
         logger.info("Complete....");
@@ -66,6 +78,7 @@ public class App {
     }
 
     public static void main(String[] args) {
-        app = new App();
+
+        app = new App(args);
     }
 }
